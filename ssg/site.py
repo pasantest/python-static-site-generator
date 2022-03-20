a40ee14a2fa5b3cf49ce8a31f1ea17b28385c0f7
pasanprem <46164432+pasanprem@users.noreply.github.com>
2022-03-20 09:09:54 +0000

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -25,8 +25,8 @@ class Site:
 				return parser
 
 	def run_parser(self, path):
-		parser = self.load_parsers(path.suffix)
-		if parser is not None:
-			parser.parse(path, self.source, self.dest)
-		else:
-			print("Not Implemented")
+        parser = self.load_parser(path.suffix)
+        if parser is not None:
+            parser.parse(path, self.source, self.dest)
+        else:
+            print("Not Implemented")
