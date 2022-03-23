b97bee23368d92e80ba42ff068805944efbc9283
pasanprem <46164432+pasanprem@users.noreply.github.com>
2022-03-23 19:50:37 +0000

    Update content.py

--- ssg/content.py
+++ ssg/content.py
@@ -25,6 +25,26 @@ class Content(Mapping):
 	@property
 	def type(self):
 			return self.data["type"] if "type" in self.data else None
+
+	@type.setter
+	def type(self,type):
+		self.data["type"] = type
+
+	def __getitem__(self, key):
+		return self.data[key]
+
+	def __iter__(self):
+		self.data.__iter__()
+
+	def __len(self):
+		return len(self.data)
+
+	def __repr__(self):
+		data = {}
+		for key, value in self.data.items():
+			if key != "content":
+				data[key] = value
+		return str(data)
 		
 
 		
